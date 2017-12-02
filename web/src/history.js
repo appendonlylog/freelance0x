525cc3fefcd589bfe4560ced677e04e9f09ac21c
Sam Kozin <sam.kozin@gmail.com>
2017-12-02 17:57:19 +0300

    Temporarily use hash history to allow deep linking
    GitHub pages doesn't support custom serving rules, so we have to resort to hash history until we deploy an intermediate router.
    
    Closes #15

--- web/src/history.js
+++ web/src/history.js
@@ -1,4 +1,6 @@
-import createHistory from 'history/createBrowserHistory'
+// Temporarily use hash history instead of history/createBrowserHistory to
+// allow deep linking as GitHub pages doesn't support custom serving rules.
+import createHistory from 'history/createHashHistory'
 
 const history = createHistory()
 
